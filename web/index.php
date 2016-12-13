c3af6307b1f4882942d9789f01e558c40e9e4e6e
Syu93 <herve.tutuaku@gmail.com>
2016-12-14 00:07:03 +0100

    [feature] file API

---
+++ web/index.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('Pinnackl\Controller', realpath(__DIR__.'/..').'/src');
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$toController = function($shortClass, $shortMethod)
+{
+    return sprintf('Pinnackl\Controller\%s::%sAction', $shortClass, $shortMethod);
+};
+
+$app->get('/', function () {
+    return "<h1>Pinnackl - CDN</h1>";
+})->bind('home');
+
+$app->get('/api/images', $toController('Cdn', 'show'));
+
+$app->match('/api/images/upload', $toController('Cdn', 'upload'));
+
+$app->run();
